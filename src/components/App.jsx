d72dbe564504db405925ad40c14ebf8ceb375d25
TalyaMonya <128909188+TalyaMonya@users.noreply.github.com>
2023-10-01 20:20:48 +0300

    Done Task4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,11 @@ import data from './Statistics/data.json';
 import { Statistics } from './Statistics/Statistics';
 
 import friends from './FriendList/friends.json';
-import {FriendList} from './FriendList/FriendList'
+import { FriendList } from './FriendList/FriendList';
+
+import transactions from './TransactionHistory/transactions.json';
+import {TransactionHistory} from './TransactionHistory/TransactionHistory'
+
 
 export const App = () => {
   return (
@@ -18,6 +22,7 @@ export const App = () => {
         stats={user.stats} />
       <Statistics stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   )
 };
